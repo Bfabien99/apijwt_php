72bc59141c7c76a816685cc07f69817fb742bcb3
bfabien99 <fabienbrou99@gmail.com>
2022-10-13 09:57:36 +0000

    creat api rest model

---
+++ rest.php
@@ -0,0 +1,48 @@
+<?php
+    class Rest{
+
+        public function __construct(){
+
+        }
+
+        /**
+         * Permet de valider la requête
+         */
+        public function validateRequest()
+        {
+
+        }
+
+         /**
+         * Permet de controler le processuce de l'api
+         */
+        public function processApi()
+        {
+
+        }
+
+        /**
+         * Permet de valider les paramètres
+         */
+        public function validateParameter($fieldName, $value, $dataType, $required)
+        {
+
+        }
+
+        /**
+         * Permet de retourner les erreurs
+         */
+        public function throwError($code, $message)
+        {
+
+        }
+
+        /**
+         * Permet de return la reponse
+         */
+        public function returnResponse()
+        {
+
+        }
+    }
+?>
