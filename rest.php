6d4225471fefa3d74425ee7056c750bb3106a020
bfabien99 <fabienbrou99@gmail.com>
2022-10-13 10:43:19 +0000

    validate request ok

--- rest.php
+++ rest.php
@@ -1,23 +1,43 @@
 <?php
+    require_once('constants.php');
     class Rest{
+        protected $request;
+        protected $serviceName;
+        protected $param;
 
-        public function __construct(){
+        public function __construct()
+        {
+            // On v"rifie la méthode de la requête
+            if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+                $this->throwError(REQUEST_METHOD_NOT_VALID, 'Request method is not valid.');
+                exit();
+            }
+
+            // On récupère les données envoyées 
             $handler = fopen('php://input', 'r');
-            echo $request = stream_get_contents($handler);
+            $this->request = stream_get_contents($handler);
+
+            // On vérifie si les données sont valides
+            $this->validateRequest($this->request);
         }
 
         /**
          * Permet de valider la requête
          */
-        public function validateRequest()
+        private function validateRequest()
         {
+            if($_SERVER['CONTENT_TYPE'] !== "application/json"){
+                $this->throwError(REQUEST_CONTENTTYPE_NOT_VALID, "Request 'content-type' is not valid. only 'application/json' allowed");
+            }
 
+            $data = json_decode($this->request, true);
+            print_r($data);
         }
 
          /**
          * Permet de controler le processuce de l'api
          */
-        public function processApi()
+        protected function processApi()
         {
 
         }
@@ -25,7 +45,7 @@
         /**
          * Permet de valider les paramètres
          */
-        public function validateParameter($fieldName, $value, $dataType, $required)
+        protected function validateParameter($fieldName, $value, $dataType, $required)
         {
 
         }
@@ -33,15 +53,17 @@
         /**
          * Permet de retourner les erreurs
          */
-        public function throwError($code, $message)
+        protected function throwError($code, $message)
         {
-
+            header("content-type:application/json");
+            $errMsg = json_encode(["error" => ['status'=>$code, 'message'=>$message]]);
+            echo $errMsg; exit();
         }
 
         /**
          * Permet de return la reponse
          */
-        public function returnResponse()
+        protected function returnResponse()
         {
 
         }
